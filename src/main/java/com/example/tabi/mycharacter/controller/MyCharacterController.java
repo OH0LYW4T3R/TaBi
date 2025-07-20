5e2974247cd951761529b0b1b5734531c5b67cfb
OH0LYW4T3R <tjdtnco@gmail.com>
2025-07-20 18:53:32 +0900

    [ADD] - My Character retrieval Swagger Documents

--- src/main/java/com/example/tabi/mycharacter/controller/MyCharacterController.java
+++ src/main/java/com/example/tabi/mycharacter/controller/MyCharacterController.java
@@ -2,6 +2,10 @@ package com.example.tabi.mycharacter.controller;
 
 import com.example.tabi.mycharacter.service.MyCharacterService;
 import com.example.tabi.mycharacter.vo.MyCharacterDto;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
@@ -15,6 +19,24 @@ import org.springframework.web.bind.annotation.RestController;
 public class MyCharacterController {
     private final MyCharacterService myCharacterService;
 
+    @Operation(
+    summary = "내 캐릭터 정보 조회",
+    description = "인증된 사용자의 보유 캐릭터 정보를 조회.",
+    responses = {
+        @ApiResponse(
+            responseCode = "200",
+            description = "조회 성공",
+            content = @Content(
+                mediaType = "application/json",
+                schema = @Schema(implementation = MyCharacterDto.class)
+            )
+        ),
+        @ApiResponse(
+            responseCode = "400",
+            description = "AppUser가 존재하지 않음"
+        )
+    }
+)
     @GetMapping("/info")
     public ResponseEntity<?> getMyCharacterInfo(Authentication authentication) {
         MyCharacterDto myCharacterDto = myCharacterService.getMyCharacter(authentication);
