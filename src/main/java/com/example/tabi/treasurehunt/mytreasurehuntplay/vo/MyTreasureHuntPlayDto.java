0119260b884317736749908429a9a68db579bce0
OH0LYW4T3R <tjdtnco@gmail.com>
2025-09-14 15:35:00 +0900

    [FIX] - MyTreasureHuntPlay Field Name / Increase Upload Image Size

--- src/main/java/com/example/tabi/treasurehunt/mytreasurehuntplay/vo/MyTreasureHuntPlayDto.java
+++ src/main/java/com/example/tabi/treasurehunt/mytreasurehuntplay/vo/MyTreasureHuntPlayDto.java
@@ -11,7 +11,7 @@ import java.time.LocalDateTime;
 @AllArgsConstructor
 @NoArgsConstructor
 public class MyTreasureHuntPlayDto {
-    private Long myTreasureHuntId;
+    private Long myTreasureHuntPlayId;
     private Long appUserId;
     private Long treasureHuntPostId;
     private PlayStatus playStatus;
